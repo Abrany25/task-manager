1f6a486b53d085ac45dfedea719d8154d1dba926
Abrany <abrany25@gmail.com>
2025-06-28 05:03:09 -0400

    feat(settings): configurada logica de jwt en el backend
    para veirificar si el token ha expirado

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +152,11 @@ SWAGGER_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
